b8c69e7bfc0de6a1ce5b78bef42cfd2cbc36c763
Daniel Heater <daniel@heatertech.com>
2019-02-23 16:23:19 -0600

    Update scripts to generate data with the filename in the data.

--- data/scripts/prep4intersect.sh
+++ data/scripts/prep4intersect.sh
@@ -2,6 +2,7 @@
 
 count=1
 for f in ../raw/*.bed; do
-    cut -f1-3 $f | sort -k1,1 -k2,2n > ../tidy/$count.bed3
+    cut -f1-3 $f | sort -k1,1 -k2,2n \
+        | awk -vid=$(basename $f) '{ print $0"\t"id; }' > ../tidy/$count.id.bed;
     let count=$count+1
 done
