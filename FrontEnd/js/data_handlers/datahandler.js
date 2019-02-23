181c6c78e13d4523ecc03d739f1820f7d5d0c988
unknown <pxp.phillips@gmail.com>
2019-02-23 15:22:29 -0600

    updating for file loading

--- FrontEnd/js/data_handlers/datahandler.js
+++ FrontEnd/js/data_handlers/datahandler.js
@@ -1,11 +1,33 @@
 const {dialog} = require('electron').remote;
+const {readFileSync} = require('fs').readFileSync;
 
 document.querySelector("#btnLoadFile").addEventListener('click', function(event){
     dialog.showOpenDialog({
-        properties:['openFile', 'multiSelections']
+        properties:['openFile' ]
     },function(files){
         if(files !== undefined){
-            console.log("files received " + files);
+            console.log("processing the files");
+
+
+            let rowData = [];
+
+            let data = readFileSync(files[0]);
+
+            let rows = data.split('\n');
+            for(let r in rows){
+                let obj = {};
+                let col = r.split('\t');
+                obj.name = col[0];
+                obj.start = col[1];
+                for(let prop in col[3]){
+                    prop.split('|');
+                    obj.end = prop[0];
+                    obj.intersections = prop[1];
+                    obj.pairmatches = prop[2];
+                }
+                rowData.push(obj);
+            }
+            console.log(`Number of rows dectected ${rowData.length}`);
         }
     }
     );
