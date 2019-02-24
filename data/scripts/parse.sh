cdf5be7b234369e9fbfbd30f73a79c90cd9ef563
Daniel Heater <daniel@heatertech.com>
2019-02-23 18:49:19 -0600

    We can specify the amount of overlap including negative!

--- data/scripts/parse.sh
+++ data/scripts/parse.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-bedops --everything ../tidy/*.bed | bedmap --echo --count --bases --delim '\t' - > ../tidy/result.bed
+N=${1:-1}
+k=${2:-1}
+prefix=${3:-../tidy}
+
+# Normalize the prefix path
+prefix=$(readlink -f $prefix)
+
+resultfile=$prefix/result.out
+
+# Determine options for overlap
+overlap="--bp-ovr $N" # Positive values
+if [ $N -le 0 ]; then
+    let N=$N*-1
+    overlap="--range $N" # Zero or negative (--range 0 is the same as --bp-ovr 1)
+fi
+
+echo "Comparing with N=" $N " k="$k " using " $overlap
+echo "Writing result to " $resultfile
+bedops --everything --ec $prefix/*.in.bed \
+    | bedmap --echo $overlap --count --bases --delim '\t' --sweep-all - > $resultfile
 
--- data/scripts/prep4parse.sh
+++ data/scripts/prep4parse.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+inputdir=${1:-../raw}
+outputdir=${2:-../tidy}
+
 count=1
-for f in ../raw/*.bed; do
+for f in $inputdir/*.bed; do
     cut -f1-3 $f | sort -k1,1 -k2,2n \
-        | awk -vid=$(basename $f) '{ print $0"\t"id; }' > ../tidy/$count.id.bed;
+        | awk -vid=$(basename $f) '{ print $0"\t"id; }' > $outputdir/$count.in.bed;
     let count=$count+1
 done
