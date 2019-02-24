490760816995270c7e0229e13f63b873acb8ad7d
Daniel Heater <daniel@heatertech.com>
2019-02-24 05:22:20 -0600

    Add some links in the comments

--- scripts/parse.sh
+++ scripts/parse.sh
@@ -18,7 +18,12 @@ fi
 echo "Comparing with N =" $N " using " $overlap
 echo "Writing result to " $resultfile
 
+# Note that we are not using the --faster option here. That would result in
+# better performance, but requires that data has no fully-nested elements.
+# See https://bedops.readthedocs.io/en/latest/content/reference/statistics/bedmap.html#using-faster-with-range for details
+#
 # --ec does an error check to confirm we have valid BED data.
 # --sweep-all prevents SIGPIPE errors
+# See https://bedops.readthedocs.io/en/latest/content/reference/statistics/bedmap.html#i-o-event-handling for details.
 bedops --everything --ec $prefix/*.in.bed \
     | bedmap --echo $overlap --count --bases --delim '\t' --sweep-all - > $resultfile
